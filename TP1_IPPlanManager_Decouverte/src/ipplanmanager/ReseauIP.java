81eb7db99f21b549d19cfaa1c5717482165e2798
KEMENE TAKO JAMES NELSON <kemenetakojamesnelson@gmail.com>
2026-05-09 14:50:28 +0200

    Ajout du TP1 IPPlan-Manager

---
+++ TP1_IPPlanManager_Decouverte/src/ipplanmanager/ReseauIP.java
@@ -0,0 +1,32 @@
+package ipplanmanager;
+
+public class ReseauIP {
+
+    String adresseReseau;
+    int masqueCidr;
+    String description;
+
+    public ReseauIP(String adresseReseau,
+                    int masqueCidr,
+                    String description) {
+
+        this.adresseReseau = adresseReseau;
+        this.masqueCidr = masqueCidr;
+        this.description = description;
+    }
+
+    public void afficher() {
+
+        System.out.println(
+            "Reseau : "
+            + adresseReseau
+            + "/"
+            + masqueCidr
+        );
+
+        System.out.println(
+            "Description : "
+            + description
+        );
+    }
+}
